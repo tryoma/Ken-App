4bbd93ee1860cf81d1c989b00f07624941622787
tryoma <tryoma0326@gmail.com>
2024-03-31 14:23:00 +0900

    修正！！！

--- src/app/private/point/page.tsx
+++ src/app/private/point/page.tsx
@@ -8,8 +8,14 @@ interface ApiResponse {
 
 async function getData() {
   logger.info(`Stripe webhook start! ${process.env.NEXT_PUBLIC_API_BASE_URL}`);
-  const response =
-    (await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`)) || {};
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/products`
+  );
+
+  if (!response.ok) {
+    // エラーハンドリングを追加
+    throw new Error('Failed to fetch data');
+  }
   const { data } = (await response.json()) as ApiResponse;
 
   return data;
@@ -19,7 +25,6 @@ const Point = async ({ searchParams }: { searchParams: any }) => {
   const data = await getData();
   const isStatus = searchParams['status'];
 
-  // return <>てすと</>;
   return <PointMain pointWithProducts={data} isStatus={isStatus ?? ''} />;
 };
 
