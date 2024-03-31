e6291691b1e8835cb29502d09974f6a6edf27927
tryoma <tryoma0326@gmail.com>
2024-03-31 16:48:49 +0900

    コミット

---
+++ generate-sw.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const path = require('path');
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'local') {
+  require('dotenv').config({ path: '.env.local' });
+}
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+};
+
+const swContent = `
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-messaging.js');
+
+// Initialize the Firebase app in the service worker by passing the generated config
+const firebaseConfig = ${JSON.stringify(firebaseConfig, null, 2)};
+
+firebase.initializeApp(firebaseConfig);
+
+// Retrieve firebase messaging
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
+`;
+
+// Write the content to firebase-messaging-sw.js in the public directory
+fs.writeFileSync(
+  path.join(__dirname, 'public', 'firebase-messaging-sw.js'),
+  swContent
+);
+console.log('Service worker generated successfully.');
