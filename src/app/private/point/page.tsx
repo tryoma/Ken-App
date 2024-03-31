a82a295776ff8e285ae2fecf3e0b5b68eb5a4276
tryoma <tryoma0326@gmail.com>
2024-03-31 16:46:07 +0900

    変更ｓ

--- src/app/private/point/page.tsx
+++ src/app/private/point/page.tsx
@@ -1,30 +1,113 @@
+'use client';
+
 import { ProductWithPrices } from '@/type';
-import PointMain from './components/PointMain';
-import logger from '../../../../logger';
+import {
+  CommonSnackbarWithRef,
+  SnackbarHandler,
+} from '@/app/components/snackbar';
+import { useAppContext } from '@/context/AppContext';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+import { useEffect, useRef, useState } from 'react';
 
-interface ApiResponse {
-  data: ProductWithPrices[];
-}
-
-async function getData() {
-  logger.info(`Stripe webhook start! ${process.env.NEXT_PUBLIC_API_BASE_URL}`);
-  const response = await fetch(`https://dev.ken-app.com/api/products`);
-
-  if (!response.ok) {
-    // エラーハンドリングを追加
-    logger.error(response);
-    throw new Error('Failed to fetch data');
-  }
-  const { data } = (await response.json()) as ApiResponse;
-
-  return data;
-}
-
-const Point = async ({ searchParams }: { searchParams: any }) => {
-  const data = await getData();
+const Point = ({ searchParams }: { searchParams: any }) => {
   const isStatus = searchParams['status'];
+  const navigation = useRouter();
+  const { userId } = useAppContext();
+  const [pointWithProducts, setPointWithProducts] = useState<
+    ProductWithPrices[] | null
+  >(null);
+  const snackbarRef = useRef<SnackbarHandler>(null);
+  const openSnackbar = (message: string) => {
+    if (snackbarRef.current) {
+      snackbarRef.current.open(message);
+    }
+  };
 
-  return <PointMain pointWithProducts={data} isStatus={isStatus ?? ''} />;
+  useEffect(() => {
+    if (isStatus === 'success') {
+      openSnackbar('購入が完了しました');
+    } else if (isStatus === 'failure') {
+      openSnackbar('購入に失敗しました');
+    }
+  }, [isStatus]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('/api/products');
+      const data = await response.json();
+      setPointWithProducts(data.data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <div className="container mx-auto p-8">
+        <div className="max-w-md mx-auto bg-white rounded p-6 shadow-md">
+          <h2 className="text-2xl font-semibold mb-6">ポイントを購入する</h2>
+          <div className="grid gap-4">
+            {pointWithProducts &&
+              pointWithProducts.map(product => (
+                <div
+                  key={product.id}
+                  className="grid gap-4 p-4 border border-gray-300 rounded-md"
+                >
+                  <div className="flex justify-between">
+                    <div className="flex items-center">
+                      <Image
+                        src={product.images[0]}
+                        alt={product.name}
+                        width={'64'}
+                        height={'64'}
+                        className="w-16 h-16 rounded-full object-cover"
+                      />
+                      <div>
+                        <div className="text-lg font-semibold">
+                          {product.name}
+                        </div>
+                        <div className="text-sm text-gray-600">
+                          {product.description}
+                        </div>
+                      </div>
+                    </div>
+                    {product.prices.map(price => (
+                      <div key={price.id}>
+                        <span>
+                          {price.unit_amount?.toLocaleString()}
+                          {price.currency.toLocaleUpperCase()}
+                        </span>
+                        <button
+                          onClick={async () => {
+                            const response = await fetch('/api/checkout', {
+                              method: 'post',
+                              headers: {
+                                'Content-Type': 'application/json',
+                              },
+                              body: JSON.stringify({
+                                price_id: price.id,
+                                user_id: userId,
+                                point: product.description,
+                              }),
+                            }).then(data => data.json());
+                            navigation.push(response.checkout_url);
+                          }}
+                          className="ml-4 px-4 py-2 bg-blue-500 text-white rounded"
+                        >
+                          購入する
+                        </button>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+      <CommonSnackbarWithRef ref={snackbarRef} />
+    </>
+  );
 };
 
 export default Point;
