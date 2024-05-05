81daffa3568a06db3bf2f93f1f587486f0608cc3
tryoma <tryoma0326@gmail.com>
2024-05-06 07:21:14 +0900

    試し

--- functions/src/functions/updateStatusAfter5Days.ts
+++ functions/src/functions/updateStatusAfter5Days.ts
@@ -1,29 +1,21 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
 export const updateStatusAfter5Days = functions
   .region('asia-northeast1')
-  .pubsub.schedule('0 1 * * *')
+  .pubsub.schedule('*/2 * * * *')
   .timeZone('Asia/Tokyo')
   .onRun(async context => {
-    console.log('This will be run every day at AM 1:00 Tokyo time!');
-    // const now = admin.firestore.Timestamp.now();
-    // const fiveDaysAgo = admin.firestore.Timestamp.fromMillis(
-    //   now.toMillis() - 5 * 24 * 60 * 60 * 1000
-    // );
-
-    // const adviceRequestRef = admin.firestore().collection('AdviceRequest');
-    // const snapshot = await adviceRequestRef
-    //   .where('createdAt', '<=', fiveDaysAgo)
-    //   .get();
-
-    // const batch = admin.firestore().batch();
-
-    // snapshot.docs.forEach(doc => {
-    //   if (doc.data().status !== 'new_status') {
-    //     // ここで必要なステータスに応じて変更してください
-    //     batch.update(doc.ref, { status: 'new_status' }); // ここで新しいステータスに更新
-    //   }
-    // });
-
-    // await batch.commit();
+    const nowTime = admin.firestore.Timestamp.now();
+    console.log({ nowTime });
+    const adviceRequests = await admin
+      .firestore()
+      .collection('AdviceRequests')
+      .where('status', '==', 'requested')
+      .where('limitTime', '<=', nowTime)
+      .get();
+    console.log({ adviceRequests });
+    adviceRequests.forEach(async doc => {
+      await doc.ref.update({ status: 'rejected' });
+    });
   });
