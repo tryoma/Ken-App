12a338ca15d35011427dac6ce0e1a735c673f19d
tryoma <tryoma0326@gmail.com>
2024-05-06 10:50:16 +0900

    修正

--- functions/src/functions/sendEmailAndFcmToUser.ts
+++ functions/src/functions/sendEmailAndFcmToUser.ts
@@ -69,6 +69,9 @@ export const sendEmailAndFcmToUser = functions
           notification: {
             title: content?.fcmTitle,
             body: fcmBody,
+            fcmOptions: {
+              link: content?.fcmClickAction,
+            },
           },
           webpush: {
             fcmOptions: {
